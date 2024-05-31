48d0b9161ae75c94937a7e0f2e166dd52831583c
urszulaak <mala.urszula@gmail.com>
2024-05-31 16:31:02 +0200

    Initialize thread

--- mutex.c
+++ mutex.c
@@ -4,7 +4,24 @@
 #include <string.h>
 #include <errno.h>
 
+pthread_mutex_t mutex;
+
+void* car(){
+    
+}
+
 int mutex(int N, char** version){
-    //kod programu
+    pthread_t cars;
+    srand(time(NULL));
+    for(int i=1;i<N+1;i++){
+        if(pthread_create(&cars, NULL, &car, NULL)!=0){
+            return -1;
+        }
+    }
+    for(int i=1;i<N+1;i++){
+        if(pthread_join(cars, NULL)!=0){
+            return -1;
+        }
+    }
     return 0;
 }
